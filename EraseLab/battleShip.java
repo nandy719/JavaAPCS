b225026426b602c854af982f5241b9fa9c5d5c4e
nandy719 <34076163+nandy719@users.noreply.github.com>
2018-11-29 08:50:28 -0800

    Add files via upload

---
+++ EraseLab/battleShip.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.Scanner;
+public class battleShip{
+    public static void main(String[] args){
+        char[][] board = new char[20][20];
+        File file = new File("digital.txt");
+        
+        
+    }
+}
